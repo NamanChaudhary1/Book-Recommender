e3d29d5c3ad36044d986c6014ef08bb75ae9164c
Naman Chaudhary <91721649+NamanChaudhary1@users.noreply.github.com>
2023-06-14 00:36:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,23 @@
 import streamlit as st
-import khtube
-khtube.checkFFmpeg()
-# Placeholder functions for downloading video and playlist videos using the hypothetical khtube library
-def download_video(link, quality, output_path):
-    st.write("Downloading video:")
-    st.write(f"Link: {link}")
-    st.write(f"Quality: {quality}")
-    st.write(f"Output Path: {output_path}")
-
-def download_playlist_videos(playlist_link, min_views=1, quality=248, start_index=1, verbose=1, output_dir="./Videos/"):
-    st.write("Downloading playlist videos:")
-    st.write(f"Playlist Link: {playlist_link}")
-    st.write(f"Min Views: {min_views}")
-    st.write(f"Quality: {quality}")
-    st.write(f"Start Index: {start_index}")
-    st.write(f"Verbose: {verbose}")
-    st.write(f"Output Directory: {output_dir}")
+from Youtube_Downloader.youtube import download
 
 def main():
-    st.title("YouTube Downloader")
+    st.title("YouTube Video Downloader")
 
-    option = st.radio("Select an option:", ("Download Single Video", "Download Playlist Videos"))
+    link = st.text_input("Enter YouTube video link:")
+    path = st.text_input("Enter the path to save the video:")
 
-    if option == "Download Single Video":
-        link = st.text_input("Enter YouTube video URL:")
-        quality = st.selectbox("Select video quality:", ["Low", "Medium", "High"])
-        output_path = st.text_input("Enter output directory:")
-
-        if st.button("Download"):
-            download_video(link, quality, output_path)
-            st.success("Video downloaded successfully!")
-
-    elif option == "Download Playlist Videos":
-        playlist_link = st.text_input("Enter YouTube playlist URL:")
-        min_views = st.number_input("Minimum views:", min_value=1, value=1)
-        quality = st.selectbox("Select video quality:", [1080, 720, 480, 360])
-        start_index = st.number_input("Start index:", min_value=1, value=1)
-        verbose = st.selectbox("Verbose level:", [1, 2, 3])
-        output_dir = st.text_input("Enter output directory:", "./Videos/")
-
-        if st.button("Download Playlist Videos"):
-            download_playlist_videos(playlist_link, min_views, quality, start_index, verbose, output_dir)
-            st.success("Playlist videos downloaded successfully!")
+    if st.button("Download"):
+        video = download(link, path)
+        video.Download_video_in_360_resolution()
+        with open(path, "rb") as f:
+            st.download_button(
+                label="Click here to download the video",
+                data=f,
+                file_name="video.mp4",
+                mime="video/mp4"
+            )
+        st.success("Video downloaded successfully!")
 
 if __name__ == "__main__":
     main()

07c29c9b3441ff56597baf7d73735477dfd93ce0
Naman Chaudhary <91721649+NamanChaudhary1@users.noreply.github.com>
2023-06-14 00:44:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,27 @@
 import streamlit as st
-from Youtube_Downloader.youtube import download
+import pytube
+from io import BytesIO
+
+def download_video(url):
+    youtube = pytube.YouTube(url)
+    video = youtube.streams.get_highest_resolution()
+    video_bytes = BytesIO()
+    video.download(output_path=video_bytes)
+    return video_bytes
 
 def main():
     st.title("YouTube Video Downloader")
 
-    link = st.text_input("Enter YouTube video link:")
-    path = st.text_input("Enter the path to save the video:")
+    video_url = st.text_input("Enter the YouTube video URL:")
 
     if st.button("Download"):
-        video = download(link, path)
-        video.Download_video_in_360_resolution()
-        with open(path, "rb") as f:
-            st.download_button(
-                label="Click here to download the video",
-                data=f,
-                file_name="video.mp4",
-                mime="video/mp4"
-            )
+        video_bytes = download_video(video_url)
+        st.download_button(
+            label="Click here to download the video",
+            data=video_bytes,
+            file_name="video.mp4",
+            mime="video/mp4"
+        )
         st.success("Video downloaded successfully!")
 
 if __name__ == "__main__":

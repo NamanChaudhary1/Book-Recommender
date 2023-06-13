12c8edf472b053a12b6236cda46f7a2722821f9e
Naman Chaudhary <91721649+NamanChaudhary1@users.noreply.github.com>
2023-06-14 01:22:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,45 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load the book dataset
-books_data = pd.read_csv("books.csv")
-
-# Create a CountVectorizer object to transform the book descriptions into a matrix of token counts
-vectorizer = CountVectorizer(stop_words="english")
-book_descriptions = vectorizer.fit_transform(books_data["description"].fillna(""))
-
-# Calculate the cosine similarity matrix
-cosine_sim = cosine_similarity(book_descriptions)
-
-def get_recommendations(title, category, num_recommendations=5):
-    # Filter books by category
-    filtered_books = books_data[books_data["category"] == category]
-
-    # Get the index of the selected book
-    indices = filtered_books.index[filtered_books["title"] == title].tolist()
-
-    if len(indices) == 0:
+# Load the CSV data from the given URL
+@st.cache
+def load_data(url):
+    try:
+        df = pd.read_csv(url)
+        return df
+    except Exception as e:
+        st.error(f"Error: {e}")
         return None
 
-    index = indices[0]
+# Get book recommendations based on selected criteria
+def get_recommendations(title, author, genre, height, publisher, data, num_recommendations=5):
+    # Filter books based on selected criteria
+    filtered_books = data.copy()
+    if title:
+        filtered_books = filtered_books[filtered_books["title"].str.contains(title, case=False)]
+    if author:
+        filtered_books = filtered_books[filtered_books["author"].str.contains(author, case=False)]
+    if genre:
+        filtered_books = filtered_books[filtered_books["genre"].str.contains(genre, case=False)]
+    if height:
+        filtered_books = filtered_books[(filtered_books["height"] >= height[0]) & (filtered_books["height"] <= height[1])]
+    if publisher:
+        filtered_books = filtered_books[filtered_books["publisher"].str.contains(publisher, case=False)]
 
-    # Calculate the pairwise similarity scores for the selected book
+    if len(filtered_books) == 0:
+        return None
+
+    # Create a CountVectorizer object to transform book descriptions into a matrix of token counts
+    vectorizer = CountVectorizer(stop_words="english")
+    book_descriptions = vectorizer.fit_transform(filtered_books["description"].fillna(""))
+
+    # Calculate cosine similarity matrix
+    cosine_sim = cosine_similarity(book_descriptions)
+
+    # Get recommendations for the first book in the filtered list
+    index = filtered_books.index[0]
     similarity_scores = list(enumerate(cosine_sim[index]))
-
-    # Sort the books based on similarity scores
     sorted_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-
-    # Get the top recommended books
     top_books_indices = [i[0] for i in sorted_scores[1:num_recommendations+1]]
 
     return filtered_books.iloc[top_books_indices]["title"]
@@ -39,19 +49,29 @@ def get_recommendations(title, category, num_recommendations=5):
 def main():
     st.title("Book Recommendation System")
 
-    # Input fields for user preferences
-    title = st.text_input("Enter the book title:")
-    category = st.selectbox("Select the book category:", books_data["category"].unique())
+    # Load the CSV data from the URL
+    url = "https://gist.githubusercontent.com/jaidevd/23aef12e9bf56c618c41/raw/5b9816dfe88e3bd83131be7278e1a13e0cbb0fde/books.csv"
+    data = load_data(url)
 
-    if st.button("Get Recommendations"):
-        recommendations = get_recommendations(title, category)
+    if data is not None:
+        # User inputs
+        st.subheader("User Inputs")
+        title = st.text_input("Enter book title (optional):")
+        author = st.text_input("Enter author name (optional):")
+        genre = st.text_input("Enter genre (optional):")
+        height = st.slider("Select book height range (cm):", min_value=0, max_value=400, value=(0, 400))
+        publisher = st.text_input("Enter publisher (optional):")
 
-        if recommendations is None:
-            st.error("Book not found. Please check the title and category.")
-        else:
-            st.success("Recommended Books:")
-            st.write(recommendations)
+        if st.button("Get Recommendations"):
+            recommendations = get_recommendations(title, author, genre, height, publisher, data)
+
+            if recommendations is None:
+                st.error("No books found based on selected criteria.")
+            else:
+                st.success("Recommended Books:")
+                st.write(recommendations)
 
 if __name__ == "__main__":
     main()
+
 

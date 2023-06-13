cc1d685a2850878b42e4a4404eba004f0fcde3a7
Naman Chaudhary <91721649+NamanChaudhary1@users.noreply.github.com>
2023-06-14 01:15:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,57 @@
 import streamlit as st
-import pytube
-import tempfile
-import os
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-def download_video(url):
-    youtube = pytube.YouTube(url)
-    video = youtube.streams.get_highest_resolution()
-    return video
+# Load the book dataset
+books_data = pd.read_csv("books.csv")
 
-def save_temporary_file(video):
-    temp_dir = tempfile.gettempdir()
-    temp_file_path = os.path.join(temp_dir, "video.mp4")
-    video.download(output_path=temp_file_path)
-    return temp_file_path
+# Create a CountVectorizer object to transform the book descriptions into a matrix of token counts
+vectorizer = CountVectorizer(stop_words="english")
+book_descriptions = vectorizer.fit_transform(books_data["description"].fillna(""))
+
+# Calculate the cosine similarity matrix
+cosine_sim = cosine_similarity(book_descriptions)
+
+def get_recommendations(title, category, num_recommendations=5):
+    # Filter books by category
+    filtered_books = books_data[books_data["category"] == category]
+
+    # Get the index of the selected book
+    indices = filtered_books.index[filtered_books["title"] == title].tolist()
+
+    if len(indices) == 0:
+        return None
+
+    index = indices[0]
+
+    # Calculate the pairwise similarity scores for the selected book
+    similarity_scores = list(enumerate(cosine_sim[index]))
+
+    # Sort the books based on similarity scores
+    sorted_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the top recommended books
+    top_books_indices = [i[0] for i in sorted_scores[1:num_recommendations+1]]
+
+    return filtered_books.iloc[top_books_indices]["title"]
 
 def main():
-    st.title("YouTube Video Downloader")
+    st.title("Book Recommendation System")
 
-    video_url = st.text_input("Enter the YouTube video URL:")
+    # Input fields for user preferences
+    title = st.text_input("Enter the book title:")
+    category = st.selectbox("Select the book category:", books_data["category"].unique())
 
-    if st.button("Download"):
-        video = download_video(video_url)
-        temp_file_path = save_temporary_file(video)
-        st.markdown(get_download_link(temp_file_path), unsafe_allow_html=True)
-        st.success("Video downloaded successfully!")
+    if st.button("Get Recommendations"):
+        recommendations = get_recommendations(title, category)
 
-def get_download_link(file_path):
-    with open(file_path, "rb") as file:
-        file_bytes = file.read()
-        base64_encoded = file_bytes.encode("base64").decode()
-        href = f'<a href="data:file/mp4;base64,{base64_encoded}" download>Click here to download the video</a>'
-        return href
+        if recommendations is None:
+            st.error("Book not found. Please check the title and category.")
+        else:
+            st.success("Recommended Books:")
+            st.write(recommendations)
 
 if __name__ == "__main__":
     main()
+
